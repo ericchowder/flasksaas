5b065e4f5e2d5d2b579f4e51ff903f11a56bf596
ericchowder <linksta7@gmail.com>
2021-06-07 18:34:48 -0400

    Created processes.txt to track steps taken on creating react app, and bootstrapped basic react navbar

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
-import TestComp from './TestComp';
+import TestComp from './components/TestComp';
+import Header from './components/Header'
 
 const test = true;
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     {
       // If test is true, render App component, if false render h1
     }
