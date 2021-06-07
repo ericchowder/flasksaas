fc75270a804d28165696050d2cf5a71575fd9831
ericchowder <linksta7@gmail.com>
2021-06-07 17:27:15 -0400

    Created new react test component

---
+++ client/src/TestComp.js
@@ -0,0 +1,11 @@
+// Functional Component
+const TestComp = () => {
+    return (
+      <div className="TestComp">
+        <h1>Test Comp Display</h1>
+      </div>
+    );
+  }
+  
+  export default TestComp;
+  
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
+import TestComp from './TestComp';
+
+const test = true;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {
+      // If test is true, render App component, if false render h1
+    }
+    {test ? <App /> : <h1>Hello from Index.js</h1>}
+    <TestComp />
   </React.StrictMode>,
   document.getElementById('root')
 );
