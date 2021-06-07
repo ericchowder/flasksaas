2b724a2a4b3314f9f973d38a69b38a899ed89ea5
ericchowder <linksta7@gmail.com>
2021-06-07 18:43:46 -0400

    Used props to create title (instead of hardcode)

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
 
-const Header = () => {
+
+// Props allows variables to be passed in from where the component is being used
+// Here, the title variable was passed in and used
+const Header = ({ title }) => {
     return (
         <Navbar bg="light" variant="light">
-            <Navbar.Brand href="/">Flask Saas</Navbar.Brand>
+            <Navbar.Brand href="/">{title}</Navbar.Brand>
         </Navbar>
     )
 };
+
+/* Long way
+const Header = (props) => {
+    // const { title } = props
+    return (
+        <Navbar bg="light" variant="light">
+            <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+        </Navbar>
+    )
+};
+*/
 
 export default Header
--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
 import TestComp from './components/TestComp';
-import Header from './components/Header'
 
 const test = true;
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
     {
       // If test is true, render App component, if false render h1
     }
