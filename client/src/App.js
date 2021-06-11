ec908e4af2c64bb91f09e4eed9b61fa2abdc5a70
ericchowder <linksta7@gmail.com>
2021-06-10 22:24:40 -0400

    Prettified App.js and clears search box after searching

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,47 @@
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
-import TestComp from './components/TestComp';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import TestComp from "./components/TestComp";
 
-const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
   // Create state variables for search words
   // Will be passed as props to Search comp
-  const [searchWord, setSearchWord] = useState('');
+  const [searchWord, setSearchWord] = useState("");
 
   // Handle searching for images
   // e is the submitted event
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(searchWord);
-    fetch(`https://api.unsplash.com/photos/random/?query=${searchWord}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${searchWord}&client_id=${UNSPLASH_KEY}`
+    )
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+        console.log(data.urls.full);
+        // clear search
+        setSearchWord("");
       })
       .catch((err) => {
         console.log(err);
-      })
-  }
+      });
+  };
 
-  console.log("searchWord auto updates:", searchWord)
+  console.log("searchWord auto updates:", searchWord);
 
   return (
     <div className="App">
-      <Header title="Flask Saas"/>
+      <Header title="Flask Saas" />
       {/* setting word and setWord properties of Search comp */}
-      <Search word={searchWord} setWord={setSearchWord} handleSubmit={handleSearchSubmit}/>
+      <Search
+        word={searchWord}
+        setWord={setSearchWord}
+        handleSubmit={handleSearchSubmit}
+      />
       <TestComp />
     </div>
   );
